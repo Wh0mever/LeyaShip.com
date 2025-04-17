036c345767b844a796ba1df6fcc1807eb7d218ad
Samohiddin Tolibov <tolibov3366@gmail.com>
2025-04-17 13:53:16 +0500

    third3 edition

--- scroll-animate.js
+++ scroll-animate.js
@@ -4,9 +4,9 @@
     window.addEventListener("scroll", () => {
         if (window.scrollY > 20) {
             navbar.classList.add("sticky");
-            logoImg.src = "../images/logo2.svg"; // your scrolled logo
+            logoImg.src = "/logo2.svg"; // your scrolled logo
         } else {
             navbar.classList.remove("sticky");
-            logoImg.src = "../images/logo.svg"; // your default logo
+            logoImg.src = "/logo.svg"; // your default logo
         }
     });
